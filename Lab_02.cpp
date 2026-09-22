f823992dee4d039b59dff724e11f3f6b00ac8e83
Vezory <zakharii.novosad.ri.2026@lpnu.ua>
2026-09-23 00:11:03 +0300

    Initial commit

---
+++ Lab_02.cpp
@@ -0,0 +1,31 @@
+// Lab_02.cpp
+// Лабораторна робота № 2.
+// Новосад Захар
+// Лінійні програми.
+// Варіант 16
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x;  // вхідний параметр (ОДЗ: x > 3)
+    double z1; // результат обчислення 1-го виразу
+    double z2; // результат обчислення 2-го виразу
+
+    cout << "x = ";
+    cin >> x;
+
+    z1 = (pow(x, 2) + 2 * x - 3 + (x + 1) * sqrt(pow(x, 2) - 9)) /
+        (pow(x, 2) - 2 * x - 3 + (x - 1) * sqrt(pow(x, 2) - 9));
+
+    z2 = sqrt((x + 3) / (x - 3));
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
